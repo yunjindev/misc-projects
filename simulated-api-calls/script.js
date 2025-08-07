9bd87e5165f680f84571223869675294e6b342c3
hunter <hunterweisenbach@me.com>
2025-08-07 13:10:50 -0400

    create script.js

---
+++ simulated-api-calls/script.js
@@ -0,0 +1,21 @@
+async function fetchData() {
+    const loadingText = document.getElementById("loading");
+    const dataText = document.getElementById("data");
+
+    loadingText.style.display = "block"
+
+    try {
+        const reponse = await new Promise((resolve, reject) => {
+            setTimeout(
+                ()=>
+                    Math.random() > 0.5 ? resolve("Data loading succesfully") : reject(), 3000);
+        })
+        loadingText.style.diplay ="none"
+        dataText.textContent = reponse;
+    } catch (error) {
+        loadingText.style.display= "none"
+        dataText.textContent ="error loading"
+    } finally {
+        loadingText.style.displa = "none"
+    }
+}
